4a65178acd2777db1cbf131551a32efc96439683
Gabriella Neri <99314986+GabN-hub@users.noreply.github.com>
2024-01-17 15:51:28 +0800

    finished game logic for best of 5

--- game.js
+++ game.js
@@ -15,6 +15,7 @@ function playRound(playerSelection, computerSelection) {
   const lowercasePlayer = playerSelection.toLowerCase();
   const playerCapitalized = lowercasePlayer.charAt(0).toUpperCase() + lowercasePlayer.slice(1);
   const pcCapitalized = computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1);
+
   // Tie
   if (lowercasePlayer === computerSelection) {
     return "It's a tie";
@@ -31,7 +32,33 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-//console.log(computerSelection); // For printing the latest computer choice
-console.log(playRound(playerSelection, computerSelection));
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  //best of 5 games
+  for (let i = 1; i <= 5; i++) {
+    const playerSelection = prompt("Enter your choice:");
+    const computerSelection = getComputerChoice();
+
+    console.log(playRound(playerSelection, computerSelection));
+
+    // Update scores based on the result of each round
+    if (playRound(playerSelection, computerSelection).includes("You win")) {
+      playerScore++;
+    } else if (playRound(playerSelection, computerSelection).includes("You lose")) {
+      computerScore++;
+    }
+  }
+
+  // Determine the winner after five rounds
+  if (playerScore > computerScore) {
+    return "Congratulations! You Win!";
+  } else if (computerScore > playerScore) {
+    return "Sorry. You lose";
+  } else {
+    return "It's a tie.";
+  }
+}
+
+console.log(game());
